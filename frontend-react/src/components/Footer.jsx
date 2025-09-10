c61d0f447778133c9b4cf638a320d36813b83414
Amaan0426 <amaanfkhatib@gmail.com>
2025-09-10 11:19:33 +0530

    homepage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+       <footer className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-secondary text-center'>&copy; 2025 Stock Prediction. All rights reserved.</p>
+       </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='pt-5 text-center bg-light-dark rounded'>
+          <h1 className='text-light'>Welcome to Stock Prediction</h1>
+          <p className='text-secondary lead'>Get started by registering an account</p>
+          <Button text = 'Login' class="btn btn-lg btn-outline-info" />
+        </div>
+      </div>
+    </>
+
+  )
+}
+
+export default Main
